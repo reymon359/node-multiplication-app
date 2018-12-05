ef2d051e06ff864ef8cc61ee1ba75a9e4364fa77
ramon morcillo <ramonmorcillocascales@gmail.com>
2018-12-05 12:09:01 +0100

    Introduciendo parámetros por consola con process.argv

--- app.js
+++ app.js
@@ -5,7 +5,16 @@
 //ahora mediante destructuracion vamos a inicializar la funcion de creararchivo de multiplicar
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
-let base = 'caa';
+
+//Aqui están los parámetros que se envian desde los comandos del terminal
+// console.log(process.argv);
+//introducimos en console node app --base=4 
+let argv = process.argv;
+let parametro = argv[2];
+
+let base = parametro.split('=')[1];
+
+console.log(base);
 
 crearArchivo(base)
     .then(archivo => console.log(`Archivo creado: ${archivo}`))
