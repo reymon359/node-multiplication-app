949b665beacd17e7ea66b389d26c62d8c25301f9
ramon morcillo <ramonmorcillocascales@gmail.com>
2018-12-05 13:32:45 +0100

    Usando el .command de Yargs -> parámetros y help.

--- app.js
+++ app.js
@@ -5,17 +5,35 @@
 //ahora mediante destructuracion vamos a inicializar la funcion de creararchivo de multiplicar
 const { crearArchivo } = require('./multiplicar/multiplicar');
 
+const argv = require('yargs')
+    .command('listar', 'Imprime en consola la tabla de multiplicar', {
+        base: {
+            demand: true, //el valor es obligatorio
+            alias: 'b' //se puede introducir como -b
+        },
+        limite: {
+            alias: 'l',
+            default: 10 //valor por defecto
+        }
+    })
+    .help()
+    .argv;
 
+
+console.log(argv.base);
+console.log(argv.limite);
+
+//------------Antes del Yargs
 //Aqui están los parámetros que se envian desde los comandos del terminal
 // console.log(process.argv);
-//introducimos en console node app --base=4 
-let argv = process.argv;
-let parametro = argv[2];
+//Si introducimos en console node app --base=4 
+//let argv2 = process.argv;
+// console.log(argv2);
+// let parametro = argv2[2];
+// let base = parametro.split('=')[1];
 
-let base = parametro.split('=')[1];
+// console.log(base);
 
-console.log(base);
-
-crearArchivo(base)
-    .then(archivo => console.log(`Archivo creado: ${archivo}`))
-    .catch(e => console.log(e));
+// crearArchivo(base)
+//     .then(archivo => console.log(`Archivo creado: ${archivo}`))
+//     .catch(e => console.log(e));
